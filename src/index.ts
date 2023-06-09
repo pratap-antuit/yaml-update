b3eb9c7c13ee347bdf574ad490bd8136a89ceba2
Pratap Patil <pratap.patil@zebra.com>
2023-06-09 15:02:53 +0530

    prod dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import yaml from 'js-yaml'
+import fs from 'fs'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function writeTo(content: any, filePath: any) {
+  const yamlString = yaml.dump(content)
+  fs.writeFileSync(filePath, yamlString, 'utf8')
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function loopThroughObjRecurs(obj: any, parseObject: any) {
+  for (const k in obj) {
+    if (typeof obj[k] === 'object' && obj[k] !== null) {
+      loopThroughObjRecurs(obj[k], parseObject[k])
+    } else if (obj.hasOwnProperty(k)) {
+      parseObject[k] = obj[k]
+    }
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const inputs: any = core.getInput('data')
+    core.info(`action input ${inputs} ...`)
+    for (const k in inputs) {
+      const yaml_data = yaml.load(fs.readFileSync(k, 'utf8'))
+      const jsonObject = JSON.stringify(yaml_data, null, 4)
+      const parseObject = JSON.parse(jsonObject)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+      loopThroughObjRecurs(inputs[k], parseObject)
+      writeTo(parseObject, k)
+    }
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
