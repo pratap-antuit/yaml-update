1f5b329aee475188d29288505499b1f508b08e2a
Pratap Patil <pratap.patil@zebra.com>
2023-06-09 16:40:52 +0530

    prod dependenciessssb

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ function loopThroughObjRecurs(obj: any, parseObject: any) {
 
 async function run(options: Options): Promise<void> {
   try {
-    const inputs: any = options.changes
+    const inputs: any = options
     core.info(`action input ${inputs} ...`)
     core.info(`aaaaaaaaaaaaaaaaa ${Object.entries(inputs)}`)
     for (const [file, values] of Object.entries(inputs)) {
